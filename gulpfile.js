eb49efe69e59edbf7687faf78eaa3ccc21484ab1
iwosz <iw0@o2.pl>
2018-05-16 20:33:04 +0200

    CSS and JS bundles

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 
 var gulp = require('gulp');
-var run = require('gulp-run-command').default;
-var projectDir = 'Web/autosolid/silex';
+var concat = require('gulp-concat');
+var projectDir = '../../../../Web/autosolid/silex';
 
 gulp.task('default', defaultTask);
 
@@ -10,33 +10,57 @@ function defaultTask(done) {
     done();
 }
 
-gulp.task('deploy', deployTask);
+gulp.task('copy', copyTask);
 
-function deployTask(done) {
+function copyTask(done) {
 
     // todo: copy app files to web server dir
 
-    gulp.src('vendor/**/*.*').pipe(gulp.dest('../../../../'+projectDir+'/vendor'));
+    gulp.src('vendor/**/*.*').pipe(gulp.dest(projectDir+'/vendor'));
 
-    gulp.src('app/**/*.*').pipe(gulp.dest('../../../../'+projectDir+'/app'));
+    gulp.src('app/**/*.*').pipe(gulp.dest(projectDir+'/app'));
 
-    gulp.src('views/**/*.*').pipe(gulp.dest('../../../../'+projectDir+'/views'));
+    gulp.src('views/**/*.*').pipe(gulp.dest(projectDir+'/views'));
 
     // todo: minify/uglify js and css
-    gulp.src('public/js/*.js').pipe(gulp.dest('../../../../'+projectDir+'/public/js'));
-    gulp.src('public/css/*.css').pipe(gulp.dest('../../../../'+projectDir+'/public/css'));
-    gulp.src('public/**/*.map').pipe(gulp.dest('../../../../'+projectDir+'/public/'));
+    gulp.src('public/js/bundle.js').pipe(gulp.dest(projectDir+'/public/js/'));
+    gulp.src('public/css/bundle.css').pipe(gulp.dest(projectDir+'/public/css'));
+    gulp.src('public/**/*.map').pipe(gulp.dest(projectDir+'/public/'));
 
-    gulp.src(['index.php','appconfig.json','composer.json','composer.lock']).pipe(gulp.dest('../../../../'+projectDir));
-
-    done();
-}
-
-gulp.task('publish', publish);
-
-function publish(done) {
-    run('phploy -l');
-    //run('phploy');
+    gulp.src(['index.php','appconfig.json','composer.json','composer.lock']).pipe(gulp.dest(projectDir));
 
     done();
 }
+
+gulp.task('bundle', bundleTask);
+
+function bundleTask() {
+    var jsFiles = [
+        'public/vendors/jquery/jquery.min.js',
+        'public/vendors/bootstrap4/bootstrap.min.js',
+        'public/vendors/imagesloaded/imagesloaded.pkgd.min.js',
+        'public/vendors/jquery.waypoints/jquery.waypoints.min.js',
+        'public/vendors/jquery.countUp/jquery.countup.min.js',
+        'public/vendors/jquery.matchHeight/jquery.matchHeight.min.js',
+        'public/vendors/owl.carousel/owl.carousel.min.js',
+        'public/vendors/menu/menu.min.js',
+        'public/vendors/smoothscroll/SmoothScroll.min.js',
+        'public/js/webui.js'
+    ];
+    gulp.src(jsFiles)
+        .pipe(concat('bundle.js'))
+        .pipe(gulp.dest('public/js'));
+
+    var cssFiles = [
+        'public/vendors/bootstrap4/bootstrap-reboot.min.css',
+        'public/vendors/bootstrap4/bootstrap.min.css',
+        'public/vendors/bootstrap4/bootstrap-grid.min.css',
+        'public/vendors/owl.carousel/owl.carousel.css',
+        'public/css/main.css'
+    ];
+    gulp.src(cssFiles)
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest('public/css'));
+}
+
+gulp.task('deploy', ['bundle', 'copy']);
